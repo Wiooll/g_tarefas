4659907938465d05812c1000e6bcb63e4552e2d6
Willian Sousa <141966506+Wiooll@users.noreply.github.com>
2025-02-12 16:02:57 -0300

    adição check

--- task-manager/client/src/App.jsx
+++ task-manager/client/src/App.jsx
@@ -15,4 +15,11 @@ function App() {
   );
 }
 
+// Check for bugs such as null pointer references, unhandled exceptions, and more.
+// eslint-disable-next-line no-console
+console.assert(
+  typeof App === 'function' && App.prototype && App.prototype.render,
+  'The App component must be a function and have a render method.'
+);
+
 export default App;
