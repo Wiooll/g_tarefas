79805acf7bce6a99be312633359f2bcda33920ae
Willian Sousa <141966506+Wiooll@users.noreply.github.com>
2025-02-15 15:14:05 -0300

    Update parseConfig.js

--- task-manager/client/src/utils/parseConfig.js
+++ task-manager/client/src/utils/parseConfig.js
@@ -1,8 +1,8 @@
 import Parse from 'parse';
 
-const PARSE_APPLICATION_ID = 'Mvti7phsJfrngsOWwqzzohEPFoFO451RDNBX51rA';
-const PARSE_JAVASCRIPT_KEY = '17Tl1kMWMHhuXyc29JERiUl3lUvOVG7vPxd0NcLB';
-const PARSE_SERVER_URL = 'https://parseapi.back4app.com/';
+const PARSE_APPLICATION_ID = 
+const PARSE_JAVASCRIPT_KEY = 
+const PARSE_SERVER_URL =
 
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_SERVER_URL;
